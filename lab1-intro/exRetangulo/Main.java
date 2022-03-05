5f16be30de9dfe6dd835f872ad1a347b2b9154aa
Borelli <bruhborelli6@gmail.com>
2022-03-05 11:13:21 -0300

    inicial

---
+++ lab1-intro/exRetangulo/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Retangulo ret1 = new Retangulo();
+        Retangulo ret2 = new Retangulo();
+
+        System.out.print("Digite o lado 1 do primeiro retangulo: ");
+        ret1.setLado1(sc.nextInt());
+        System.out.print("Digite o lado 2 do primeiro retangulo: ");
+        ret1.setLado2(sc.nextInt());
+        System.out.print("Digite o lado 1 do segundo retangulo: ");
+        ret2.setLado1(sc.nextInt());
+        System.out.print("Digite o lado 2 do segundo retangulo: ");
+        ret2.setLado2(sc.nextInt());
+
+        ret1.getLado1();
+        ret2.getLado2();
+
+        System.out.println("\nRetangulo 1: \narea = " + ret1.area() + "\nperimetro = " + ret1.perimetro()); 
+        System.out.println("\nRetangulo 2: \narea = " + ret2.area() + "\nperimetro = " + ret2.perimetro());
+    }
+}
---
+++ lab1-intro/exRetangulo/Retangulo.java
@@ -0,0 +1,26 @@
+public class Retangulo {
+    private int lado1, lado2;
+
+    public void setLado1(int la){
+        this.lado1 = la;
+    }
+    public int getLado1(){
+        return this.lado1;
+    }
+
+    public void setLado2(int la){
+        this.lado2 = la;
+    }
+    public int getLado2(){
+        return this.lado2;
+    }
+
+    public int area(){
+        return lado1 * lado2;
+    }
+
+    public int perimetro(){
+        return lado1 + lado2;
+    }
+
+}
